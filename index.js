4f4c14659a7b755b0f79a44d5c7851019159f5e5
Fernando Martín García Del Angel <martingarciadelangel@me.com>
2021-05-09 13:44:03 -0500

    Update index.js
    - Added Vaccination Status 'enum'
    - Added Validation Status 'enum'
    - Added a 'createPassport' method

--- index.js
+++ index.js
@@ -8,6 +8,20 @@
 const shim = require('fabric-shim')
 const util = require('util')
 
+const VaccinationStatus  = {
+    notVaccinated = "NOTVACCINATED",
+    firstDose = "FIRSTDOSE",
+    fullyVaccinated = "FULLYVACCINATED",
+    reinforcement = "REINFORCEMENT"
+}
+
+const ValidationStatus =  {
+    notValidated = "NOTVALIDATED",
+    inValidation = "INVALIDATION",
+    invalid = "INVALID",
+    valid = "VALID"
+}
+
 let Chaincode = class {
     /**
      * Chaincode Init
@@ -42,6 +56,40 @@ let Chaincode = class {
             return shim.error(err)
         }
     }
+
+    /**
+     * Creates a vaccination card on the ledger
+     * @param {Objecg} stub chaincode stub
+     * @param {Object} args initial arguments
+     * @param {Class} thisClass verbatim
+     */
+    async createVaccinationPassport(stub, args, thisClass) {
+        console.info(' --- Started createVaccinationPassport --- ')
+        let vaccinationCard = {}
+
+        vaccinationCard.id = args[0]
+        // Check if the vaccination card already exists
+        let vaccinationCardState = await stub.getState(vaccinationCard.id) 
+        if (vaccinationCardState.toString()) {
+            throw new Error(' --- Card already exists ---')
+        }
+        vaccinationCard.firstName = args[1]
+        vaccinationCard.lastName = args[2]
+        vaccinationCard.dob = args[3]
+        vaccinationCard.s3URL = args[4]
+        vaccinationCard.patientNumber = args[5]
+        vaccinationCard.country = args[6]
+        vaccinationCard.validated = ValidationStatus.notValidated
+
+        // Save to state
+        await stub.putState(vaccinationCard.id, Buffer.from(JSON.stringify(vaccinationCard)))
+        // Save an index for faster retrieval
+        let indexName = 'country~id'
+        let countryIdIndexKey = await stub.createCompositeKey(indexName, [vaccinationCard.country, vaccinationCard.id])
+        await stub.putState(countryIdIndexKey, Buffer.from('\u0000'))
+        console.info(' --- end createVaccinationPassport ---')
+    }
+
 }
 
 shim.start(new Chaincode())
