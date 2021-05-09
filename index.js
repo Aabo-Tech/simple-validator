390fb55e681af14828888e7a3c5d91fad93e2240
Fernando Martín García Del Angel <martingarciadelangel@me.com>
2021-05-09 14:16:38 -0500

    Update index.js
    - Added some styling to this code
    - Added some validation and vax status

--- index.js
+++ index.js
@@ -8,14 +8,14 @@
 const shim = require('fabric-shim')
 const util = require('util')
 
-const VaccinationStatus  = {
+const VaccinationStatus = {
     notVaccinated = "NOTVACCINATED",
     firstDose = "FIRSTDOSE",
     fullyVaccinated = "FULLYVACCINATED",
     reinforcement = "REINFORCEMENT"
 }
 
-const ValidationStatus =  {
+const ValidationStatus = {
     notValidated = "NOTVALIDATED",
     inValidation = "INVALIDATION",
     invalid = "INVALID",
@@ -42,12 +42,12 @@ let Chaincode = class {
      * @returns {Boolean} Execution Status
      */
     async Invoke(stub) {
-        console.info('Transaction ID:',stub.getTxId())
+        console.info('Transaction ID:', stub.getTxId())
         console.info(util.format('Args: %j', stub.getArgs()))
         // Get the expected method and execute it
         let ret = stub.getFunctionAndParameters()
         let methodToExecute = this[ret.fcn]
-        if (!methodToExecute) { throw new Error('Received unknown method with name:',methodToExecute)}
+        if (!methodToExecute) { throw new Error('Received unknown method with name:', methodToExecute) }
         try {
             let payload = await methodToExecute(stub, ret.params, this)
             return shim.success(payload)
@@ -69,7 +69,7 @@ let Chaincode = class {
 
         passport.id = args[0]
         // Check if the vaccination card already exists
-        let vaccinationCardState = await stub.getState(passport.id) 
+        let vaccinationCardState = await stub.getState(passport.id)
         if (vaccinationCardState.toString()) {
             throw new Error(' --- Card already exists ---')
         }
@@ -79,7 +79,9 @@ let Chaincode = class {
         passport.s3URL = args[4]
         passport.patientNumber = args[5]
         passport.country = args[6]
+        passport.dynamoHash = args[7]
         passport.validated = ValidationStatus.notValidated
+        passport.vaxStatus = VaccinationStatus.notVaccinated
 
         // Save to state
         await stub.putState(passport.id, Buffer.from(JSON.stringify(passport)))
@@ -87,31 +89,147 @@ let Chaincode = class {
         let indexName = 'country~id'
         let countryIdIndexKey = await stub.createCompositeKey(indexName, [passport.country, passport.id])
         await stub.putState(countryIdIndexKey, Buffer.from('\u0000'))
-        console.info(' --- end createVaccinationPassport ---')
+        console.info(' --- Ended createVaccinationPassport ---')
     }
 
     /**
-     * 
-     * @param {*} stub 
-     * @param {*} args 
-     * @param {*} thisClass 
+     * Reads Passport state
+     * @param {*} stub chaincode stub
+     * @param {*} args arguments
+     * @param {*} thisClass verbatim
      */
     async readPassport(stub, args, thisClass) {
+        console.info(' --- Started readPassport --- ')
         // Input Validation
-        if(args.length != 1) {
+        if (args.length != 1) {
             throw new Error('Incorrect number of arguments, expecting ID')
         }
         // Get and Validate ID
         let ID = args[0]
-        if (!ID) { throw new Error('ID must not be empty')}
+        if (!ID) { throw new Error('ID must not be empty') }
         // Query the ledger
         let passportAsBytes = await stub.getState(ID)
-        if(!passportAsBytes.toString()) {
+        if (!passportAsBytes.toString()) {
             throw new Error('Passport does not exist')
         }
+        console.info(' --- Ended readPassport --- ')
         return passportAsBytes
     }
 
+    /**
+     * Mutates validation status
+     * @param {Object} stub shim
+     * @param {Object} args arguments
+     * @param {Class} thisClass verbatim 
+     */
+    async changeValidationStatus(stub, args, thisClass) {
+        console.info(' --- Started changeValidationStatus --- ')
+        let ID = args[0]
+        let newStatus = args[2]
+        // Query for passport
+        let passportAsBytes = await stub.getState(ID)
+        if (!passportAsBytes || !passportAsBytes.toString()) {
+            throw new Error(' Passport does not exist')
+        }
+        // Create passport object
+        let passport = {}
+        try {
+            passport = JSON.parse(passportAsBytes.toString())
+        } catch (error) {
+            throw new Error(error)
+        }
+        // Change its state
+        passport.validated = newStatus
+        // Write it back
+        let passportJSONAsBytes = Buffer.from(JSON.stringify(passport))
+        await stub.putState(ID, passportJSONAsBytes)
+        console.info(' --- Ended changeValidationStatus --- ')
+    }
+
+    /**
+     * Changes vaccination status
+     * @param {Object} stub shim
+     * @param {Object} args arguments
+     * @param {Class} thisClass verbatim
+     */
+    async changeVaxStatus(stub, args, thisClass) {
+        console.info(' --- Started changeVaxStatus --- ')
+        let ID = args[0]
+        let newStatus = args[2]
+        // Query for passport
+        let passportAsBytes = await stub.getState(ID)
+        if (!passportAsBytes || !passportAsBytes.toString()) {
+            throw new Error(' Passport does not exist')
+        }
+        // Create passport object
+        let passport = {}
+        try {
+            passport = JSON.parse(passportAsBytes.toString())
+        } catch (error) {
+            throw new Error(error)
+        }
+        // Change its state
+        passport.vaxStatus = newStatus
+        // Write it back
+        let passportJSONAsBytes = Buffer.from(JSON.stringify(passport))
+        await stub.putState(ID, passportJSONAsBytes)
+        console.info(' --- Ended changeVaxStatus --- ')
+    }
+
+    async getAllResults(iterator, isHistory) {
+        let allResults = []
+        while (true) {
+            let res = await iterator.next()
+            if (res.value && res.value.value.toString()) {
+                let jsonRes = {}
+                console.log(res.value.value.toString('utf8'))
+                if (isHistory && isHistory === true) {
+                    jsonRes.TxId = res.value.tx_id
+                    jsonRes.Timestamp = res.value.timestamp
+                    jsonRes.IsDelete = res.value.is_delete.toString()
+                    try {
+                        jsonRes.Value = JSON.parse(res.value.value.toString('utf8'))
+                    } catch (err) {
+                        console.error(err)
+                        jsonRes.Value = res.value.value.toString('utf8')
+                    }
+                } else {
+                    jsonRes.Key = res.value.key
+                    try {
+                        jsonRes.Record = JSON.parse(res.value.value.toString('utf8'))
+                    } catch (err) {
+                        console.error(err)
+                        jsonRes.Value = res.value.value.toString('utf8')
+                    }
+                }
+                allResults.push(jsonRes)
+            }
+            if (res.done) {
+                console.log('end of data')
+                await iterator.close()
+                return allResults
+            }
+        }
+    }
+
+    /**
+     * Gets complete history for passport
+     * @param {Object} stub Shim
+     * @param {Object} args Method's Argument
+     * @param {Class} thisClass verbatim
+     */
+    async getHistoryForPassport(stub, args, thisClass) {
+        if (args.length < 1) {
+            throw new Error('Incorrect number of arguments, expected one')
+        }
+        // Get the passport
+        let ID = args[0]
+        console.info(' --- Started getHistoryForPassport --- ')
+        let resultsIterator = await stub.getHistoryForKey(ID)
+        let method = thisClass['getAllResults']
+        let results = await method(resultsIterator, true)
+        return Buffer.from(JSON.stringify(results))
+    }
 }
 
 shim.start(new Chaincode())
